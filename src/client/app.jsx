55785f2d609043d19fea6bce0606bb77dc9c3226
Zachary Raney <raneyzb@gmail.com>
2016-12-22 15:43:42 -0500

    Update textmod to properly use React.Component

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,17 +1,60 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Layouts from '../shared/layouts';
 import TextMod from '../shared/modules/textMod';
 
 const layouts = new Layouts();
-const textMod = new TextMod(
-  'APP ANALYTICS',
-  'Regal Cinemas',
-  'In the month of September, 30 million people used the Regal Cinemas app. This is a 20% increase from the previous months of the summer.',
-  'http://placehold.it/960x540'
-);
+const textMod = {
+  name: 'APP ANALYTICS',
+  title: 'Regal Cinemas',
+  body: 'In the month of September, 30 million people used the Regal Cinemas app. This is a 20% increase from the previous months of the summer.',
+  bgImg: 'http://placehold.it/960x540',
+  width: 960,
+  height: 540,
+};
 
 ReactDOM.render(layouts.default, document.querySelector('.table'));
-ReactDOM.render(textMod.getView(960, 540), document.querySelector('.mod1'));
-ReactDOM.render(textMod.getView(960, 540), document.querySelector('.mod2'));
-ReactDOM.render(textMod.getView(960, 540), document.querySelector('.mod3'));
-ReactDOM.render(textMod.getView(960, 540), document.querySelector('.mod4'));
+ReactDOM.render(
+  <TextMod
+    name={textMod.name}
+    title={textMod.title}
+    body={textMod.body}
+    bgImg={textMod.bgImg}
+    width={textMod.width}
+    height={textMod.height}
+  />,
+  document.querySelector('.mod1')
+);
+ReactDOM.render(
+  <TextMod
+    name={textMod.name}
+    title={textMod.title}
+    body={textMod.body}
+    bgImg={textMod.bgImg}
+    width={textMod.width}
+    height={textMod.height}
+  />,
+  document.querySelector('.mod2')
+);
+ReactDOM.render(
+  <TextMod
+    name={textMod.name}
+    title={textMod.title}
+    body={textMod.body}
+    bgImg={textMod.bgImg}
+    width={textMod.width}
+    height={textMod.height}
+  />,
+  document.querySelector('.mod3')
+);
+ReactDOM.render(
+  <TextMod
+    name={textMod.name}
+    title={textMod.title}
+    body={textMod.body}
+    bgImg={textMod.bgImg}
+    width={textMod.width}
+    height={textMod.height}
+  />,
+  document.querySelector('.mod4')
+);
